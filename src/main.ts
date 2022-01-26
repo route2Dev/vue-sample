ecb047e455340f6b10b9f6954945260df753b59f
DAVE BAUMANN <dave.baumann@route2solutions.com>
2022-01-26 09:48:47 -0600

    jdi - add composition api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import CompositionApi from '@vue/composition-api';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+Vue.use(CompositionApi);
 
 new Vue({
   router,
